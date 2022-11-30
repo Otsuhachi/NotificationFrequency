c76b76457b0f34cf3b709755f2d9dc330da3ea8d
Otsuhachi <nkzm4102@ezweb.ne.jp>
2022-11-30 23:09:34 +0900

    ver 2022.11.30
    - バリデータ、コンバータの利便性を向上させました。

--- otsunotificationfrequency/validator.py
+++ otsunotificationfrequency/validator.py
@@ -9,12 +9,15 @@ try:
 
     from . import NotificationFrequency
 
-except ImportError as e:
+except ImportError:
     msg = 'このモジュールは"otsuvalidator"をインストールしている場合のみ使用できます。'
     raise ImportError(msg)
 
 
 class VNotificationFrequency(Validator):
+
+    def __get__(self, instance, otype) -> NotificationFrequency:
+        return super().__get__(instance, otype)
 
     def validate(self, value: Any) -> NotificationFrequency:
         if type(value) is not NotificationFrequency:
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('LICENSE', 'r', encoding='utf-8') as f:
 info = sys.version_info
 setup(
     name='otsunotificationfrequency',
-    version='1.0.0',
+    version='2022.11.30',
     url='https://github.com/Otsuhachi/NotificationFrequency',
     description='要素数が確定されたシーケンスの途中で処理を挟むタイミングの判定を補助します。',
     long_description_content_type='text/markdown',
@@ -18,11 +18,6 @@ setup(
     author_email='agequodagis.tufuiegoeris@gmail.com',
     packages=find_packages(),
     include_package_data=True,
-    classifiers=[
-        'Development Status :: 1 - Planning', 'Natural Language :: Japanese', 'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8', 'Programming Language :: Python :: 3.9', 'Operating System :: Microsoft :: Windows :: Windows 10',
-        'License :: OSI Approved :: MIT License'
-    ],
     license=lcs,
-    keywords='Python Notification Frequency for',
+    keywords='Python Notification Frequency',
 )
