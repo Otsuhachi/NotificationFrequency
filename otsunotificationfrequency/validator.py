a0da543a35ff42c0fabac4a942d5d440ed49795d
Otsuhachi <nkzm4102@ezweb.ne.jp>
2023-01-14 08:01:24 +0900

    ver 1.0.1
    - バージョン表記を訂正しました。
    - enumerate感覚で扱えるメソッドを追加しました。
    - Python 3.7で動作するよう修正しました。

--- otsunotificationfrequency/validator.py
+++ otsunotificationfrequency/validator.py
@@ -1,13 +1,21 @@
+"""NotificationFrequency用のバリデータとコンバータが定義されたモジュールです。
+
+`otsuvalidator`ライブラリをインストールしている人向けのモジュールです。
+インストールされていない場合にはImportErrorが発生します。
+"""
+
 __all__ = (
-    'CNotificationFrequency',
-    'VNotificationFrequency',
+    "CNotificationFrequency",
+    "VNotificationFrequency",
 )
+
+
 try:
     from typing import Any
 
     from otsuvalidator.bases import Converter, Validator
 
-    from . import NotificationFrequency
+    from .classes import NotificationFrequency
 
 except ImportError:
     msg = 'このモジュールは"otsuvalidator"をインストールしている場合のみ使用できます。'
@@ -15,25 +23,30 @@ except ImportError:
 
 
 class VNotificationFrequency(Validator):
+    """オブジェクトがNotificationFrequencyか確認するバリデータです。"""
 
     def __get__(self, instance, otype) -> NotificationFrequency:
         return super().__get__(instance, otype)
 
     def validate(self, value: Any) -> NotificationFrequency:
         if type(value) is not NotificationFrequency:
-            msg = self.ERRMSG('NotificationFrequency型である必要があります', value)
+            msg = self.ERRMSG("NotificationFrequency型である必要があります", value)
             raise TypeError(msg)
         return value
 
 
 class CNotificationFrequency(VNotificationFrequency, Converter):
+    """VNotificationFrequencyの拡張バリデータです。
+
+    NotificationFrequency型に変換可能なオブジェクトは例外を投げずに変換を試みます。
+    """
 
     def validate(self, value: Any) -> NotificationFrequency:
         if type(value) is not NotificationFrequency:
             try:
                 value = NotificationFrequency(value)
             except:
-                msg = self.ERRMSG('NotificationFrequency型として扱える必要があります', value)
+                msg = self.ERRMSG("NotificationFrequency型として扱える必要があります", value)
                 raise TypeError(msg)
         return super().validate(value)
 
